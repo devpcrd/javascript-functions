64ed28348ad73315e9eb371b9bf8dae7cd02223d
NAME <mail@gmail.com>
2021-12-20 14:31:53 +0100

    almost

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,19 +1,127 @@
-function seed() {}
+function seed() {
+  let seeds = [];
+  for(let i = 0; i<arguments.length; i++){
+    seeds.push(arguments[i]);
+  }
+  return seeds;
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  const [xValue,yValue] = [x,y];
+  const [jValue,kValue] = [j,k];
+  if((xValue===jValue) && (yValue === kValue)){
+    return true;
+  } 
+  return false;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  const [xValue,yValue] = cell;
+  for(i=0; i<this.length;i++){
+    let [x,y] = this[i];
+    if((x===xValue) && (y===yValue)){
+      return true;
+    }
+  }
+  return false;
+}
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
 
-const corners = (state = []) => {};
+  if(contains.call(state,cell)){
+    return '\u25A3';
+  }
+  return '\u25A2';
+};
 
-const printCells = (state) => {};
+const corners = (state = []) => {
 
-const getNeighborsOf = ([x, y]) => {};
+  let xCord = [];
+  let yCord = [];
 
-const getLivingNeighbors = (cell, state) => {};
+  let corner = {
+    topRight: [0,0],
+    bottomLeft: [0,0]
+  };
+
+  if(state.length){   
+    state.forEach(element=>{
+      let [x,y] = element;
+      xCord.push(x);
+      yCord.push(y);
+    });
+    let minX = Math.min(...xCord);
+    let maxX = Math.max(...xCord);
+    let minY = Math.min(...yCord);
+    let maxY = Math.max(...yCord);
+
+    corner.topRight = [maxX,maxY];
+    corner.bottomLeft = [minX,minY];
+    return corner;
+  }
+  
+  return corner;
+};
+
+const printCells = (state) => {
+
+  let {topRight,bottomLeft} = corners(state);
+  let [xTop,yTop] = topRight
+  let [xBottom,yBottom] = bottomLeft;
+  
+  let output = "";
+ 
+  
+  for(let i = yTop; i>=yBottom; i--){
+    let row = [];
+    for(let j = xBottom; j<=xTop; j++){
+      row.push(printCell([j,i],state));
+    } 
+    output += row.join(" ") + "\n";
+  }
+
+  return output;
+};
+
+const getNeighborsOf = ([x, y]) => {
+
+  let xMax = x+1;
+  let yMax = y+1;
+  let yMin = y-1;
+  let xMin = x-1;
+
+  let neighborSet = [];
+
+  for(let i = xMin;i<=xMax; i++){
+    for(let j = yMin; j<=yMax; j++){
+      let values = [i,j];
+      neighborSet.push(values);
+    }
+  }
+
+  let output = neighborSet.filter(element=>{
+    if(element[0]===x && element[1] === y){
+      return false;
+    }
+    return true;
+  });
+
+  return output;
+
+};
+
+const getLivingNeighbors = (cell, state) => {
+  let neighboursOf = getNeighborsOf(cell);
+  let container = contains.bind(state);
+  let livingNeighbors = neighboursOf.filter(element=>{
+    if(container.call(state,element)){
+      return true;
+    }
+    return false;
+  });
+  return livingNeighbors;
+};
 
 const willBeAlive = (cell, state) => {};
 
